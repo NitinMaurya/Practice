63f60adc0a7bdae7e23a47e67e594fa491828d7a
Nitin Maurya <nitin.maurya1997@gmail.com>
2023-02-19 17:40:33 +0400

    added subset sum problem

---
+++ src/main/java/practice/DSA/algorithms/dp/SubsetSum.java
@@ -0,0 +1,65 @@
+package practice.DSA.algorithms.dp;
+
+import java.util.Arrays;
+
+public class SubsetSum {
+  public static void main(String[] args) {
+    String description = """
+        Given an array of non-negative integers, and a value sum, determine if there is a subset of the given set with
+         sum equal to given sum.
+         
+         Input:
+         N = 6
+         arr[] = {3, 34, 4, 12, 5, 2}
+         sum = 9
+         
+         Output: 1
+         
+         Explanation: Here there exists a subset with
+         sum = 9, 4+3+2 = 9.
+        """;
+
+    int[] arr = new int[]{3, 34, 4, 12, 5, 2};
+    int sum = 9;
+
+    boolean result = new Solution().solve(arr, arr.length, sum);
+
+    System.out.println("arr: " + Arrays.toString(arr) + " sum: " + sum + " output: " + result);
+
+    sum = 30;
+    result = new Solution().solve(arr, arr.length, sum);
+
+    System.out.println("arr: " + Arrays.toString(arr) + " sum: " + sum  + " output: " + result);
+  }
+}
+
+class Solution {
+
+  public boolean solve(int[] arr, int N, int sum) {
+    boolean[][] dp = new boolean[N+1][sum+1];
+
+    for (int n = 0; n < 1; n++) {
+      for (int s = 0; s < 1; s++) {
+        if (n == 0) {
+          dp[n][s] = false;
+        }
+        if (s == 0) {
+          dp[n][s] = true;
+        }
+      }
+    }
+
+    for (int n = 1; n <= N; n++) {
+      for (int s = 1; s <= sum; s++) {
+        if (arr[n-1] <= s) {
+          dp[n][s] = dp[n-1][s - arr[n-1]] || dp[n-1][s];
+        } else {
+          dp[n][s] = dp[n-1][s];
+        }
+      }
+    }
+
+    return dp[N][sum];
+  }
+
+}
---
+++ src/main/java/practice/DSA/sparseTable/SparseTable.java
@@ -0,0 +1,14 @@
+package practice.DSA.sparseTable;
+
+public class SparseTable {
+
+  public static void main(String[] args) {
+    String description = """
+        This data structure is used to answer the range minimum queries in O(1).
+        Given Preconditions: 
+          - The array must be immutable
+          - The operation must be idempotent e.g. minimum(), maximum(), gcd()
+          which means the result is not affected by the overlapped intervals
+        """;
+  }
+}
